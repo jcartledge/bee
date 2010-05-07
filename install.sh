f23c3395a546a865500dd1c9c49497f55ff4cc15
James Cartledge <jcartledge@gmail.com>
2010-05-07 11:34:33 +1000

    install script for cygwin

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env sh
-if [ "$TERM" == cygwin]; then
-  ln -s `pwd`/src/bee-cygwin  /usr/local/bin/bee
-  ln -s `pwd`/src/bee         /usr/local/bin/bee.php
-else
-  sudo ln -s `pwd`/src/bee    /usr/local/bin/bee
+#!/usr/bin/env bash
+if [ "$TERM" == "cygwin" ]
+  then
+    ln -s `pwd`/src/bee-cygwin  /usr/local/bin/bee
+    ln -s `pwd`/src/bee         /usr/local/bin/bee.php
+  else
+    sudo ln -s `pwd`/src/bee    /usr/local/bin/bee
 fi
